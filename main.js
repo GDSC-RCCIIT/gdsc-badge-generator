da055d1197b5804288fa1047ae702dbc40d5811d
Subham-dury <karchowdhurysubham@gmail.com>
2021-10-01 02:36:35 +0530

    Download Added

--- main.js
+++ main.js
@@ -7,3 +7,25 @@ var loadName = (event) => {
     let name = document.getElementById("name");
     name.innerHTML = event.target.value;
 }
+
+const btn = document.getElementById("card");
+var getCanvas;
+
+const downloadImg = () =>{
+    var card = document.getElementById("card");
+    var val = document.getElementById("name").innerHTML;
+    
+    
+
+    html2canvas(card).then(function(canvas) {
+		
+        var link = document.createElement("a");
+        document.body.appendChild(link);
+        link.download = `${val}.png`;
+        link.href = canvas.toDataURL("image/png");
+        link.target = '_blank';
+        link.click();
+    });
+}
+
+
